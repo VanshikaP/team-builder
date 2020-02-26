e49aa6231f8b07c11addd16624ef5398652935e6
VanshikaP <vanshikapundir92@gmail.com>
2020-02-26 14:18:57 -0800

    Styling Changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
 
   const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);;
 
   useEffect(() => {
     const results = players.filter(player => player.name.toLowerCase().includes(searchTerm.toLowerCase()))
@@ -29,7 +29,7 @@ function App() {
   return (
     <div className="App">
       <div className = 'header'>
-        <img className = 'logo' src = 'https://www8.lunapic.com/editor/p-trans.php?preview=1&fuzz=8&x=135&y=79&rand=5084&fill=' alt='navarro logo'></img>
+        <img className = 'logo' src = 'https://upload.wikimedia.org/wikipedia/en/thumb/0/01/Golden_State_Warriors_logo.svg/1200px-Golden_State_Warriors_logo.svg.png' alt='Golden State Warriors logo'></img>
         <div className='search'>
           <label htmlFor='search'>Search</label>
           <input type = 'text' id='search' value={searchTerm}  onChange={handleSearch} />
